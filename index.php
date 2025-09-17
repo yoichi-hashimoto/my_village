be48538bb87272a7c054a3706a8c760135c67462
hakaiou <hakaiou20067@gmail.com>
2025-09-17 11:41:45 +0900

    ホームページ作成テスト

---
+++ index.php
@@ -0,0 +1,34 @@
+<?php
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TopPage</title>
+    <link rel="stylesheet" href="index.css">
+</head>
+<body>
+    <header>
+        <img src="" alt="headerLogo">
+        <h1>ハッシー村長塾</h1>
+    </header>
+    <main>
+        <p>ハッシー村長塾へようこそ！</p>
+        <p>ここでは、移住を考えて新しい生活をスタートしようとする人たちの成長を様々な角度からサポートしていきます。</p>
+    </main>
+    <footer>
+        <p>&copy; 2025 ハッシー村長塾</p>
+        <nav>
+            <a href="#">Home</a> |
+            <a href="#">About</a> |
+            <a href="#">Contact</a>
+        </nav>
+        <p>お問い合わせは<a href="#">こちら</a>からどうぞ。</p>
+    </footer>
+    
+</body>
+</html>
